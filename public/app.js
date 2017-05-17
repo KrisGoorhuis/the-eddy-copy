51f65641ef0773dcbe20b82806a8ac5b7a5d0e10
Kris Goorhuis <krisgoorhuis@gmail.com>
2017-05-17 12:02:56 -0700

    ...

--- public/app.js
+++ public/app.js
@@ -23,8 +23,7 @@ $(document).ready(function() {
       
       var windowHeight = $(window).height();
       var newMarginTop = windowHeight-120;
-      $('#sideInfoHead').css('margin-top')
-      $('#schedule').css('margin-top', newMarginTop + "px");
+      //$('#schedule').css('margin-top', newMarginTop + "px");
    }
    
    
